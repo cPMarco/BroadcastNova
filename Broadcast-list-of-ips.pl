0563cd7635d4848a1a0ff4ac9d7486fdfe2bcfe2
Marco Ferrufino <marco@cpanel.net>
2022-02-12 20:20:57 -0600

    Switch to perl. Separate core osascript to smaller script.
    Core helper script seemed best to remain shell:
    scripts/osascript.sh
    
    Original bash script is left there, not sure what the final language
    will be. So right now either can be used.

---
+++ Broadcast-list-of-ips.pl
@@ -0,0 +1,171 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+
+use Getopt::Long;
+use File::Basename;
+
+#<<<
+Getopt::Long::GetOptions(
+    \my %opt,
+    'input_file=s',
+    'all_vms|a',
+    'sandbox_vms|s',
+    'binary_vms|b',
+    'idev_vms|i',
+    'verbose|v',
+    'help|h',
+    'man',
+) or die('[error] Use -h for help');
+#>>>
+
+# If I'm gona redo all that prunning again, here's the start
+# nova list --fields 'name','created','status','networks','metadata'
+my $FILE        = $opt{'file'} // "$ENV{'HOME'}/.config/broadcastnova/input-servers.txt";
+my $TEMP_FILE   = "/tmp/BroadcastNova-IPs.txt";
+my $USERNAME    = 'root';
+my $SCRIPT_NAME = File::Basename::basename($0);
+
+sub usage() {
+    (
+        my $help_txt = qq{
+        Usage:\n  $SCRIPT_NAME [-h|-a|-s|-b|-i|-v] ( [-f <filepath>] )
+
+        Optional Options:
+        -h This help
+        -v Cause $SCRIPT_NAME to be verbose
+        -f Use custom file for input.  Default is:
+            ${FILE}
+
+        Required Options (only one):
+        -a SSH into all VM's
+        -s SSH into only sandbox VM's
+        -b SSH into only binary VM's
+        -i SSH into only idev VM's
+
+        }
+    ) =~ s/^ {8}//mg;
+    print $help_txt;
+
+    exit(1);
+}
+
+Pod::Usage::pod2usage( -exitval => 0, -verbose => 2, noperldoc => 1 ) if ( $opt{'man'} );
+usage()                                                               if ( $opt{'help'} );
+
+sub print_out {
+    my ($text) = @_;
+    print "$text\n" if $opt{'verbose'};
+}
+
+# Main
+print_out "\nFILE: [$FILE]";
+
+# Check for Default Arguments (Instance Name);
+unless ( -r $FILE ) {
+    print -e "\n[error] FILE not found: $FILE";
+    print "[error] Ensure the directory and file exist.";
+    usage();
+}
+
+my @lines = get_lines($FILE);
+validate_num_lines(@lines);
+
+my @ips;
+if ( $opt{'all_vms'} ) {
+    @ips = get_ips( lines => \@lines, exclude => 'store|manage|verify|tickets' );
+}
+elsif ( $opt{'sandbox_vms'} ) {
+    @ips = get_ips( lines => \@lines, include => 'sand|sb-' );
+}
+elsif ( $opt{'binary_vms'} ) {
+    @ips = get_ips( lines => \@lines, exclude => 'sand|sb-|_sb_|store|manage|verify|tickets' );
+}
+elsif ( $opt{'idev_vms'} ) {
+    @ips = get_ips( lines => \@lines, include => 'store|manage|verify|tickets' );
+}
+else {
+    print -e "\n[error] type of VM is required but was not found.\n";
+    usage();
+}
+
+print "\nHost IP's found:\n";
+print "$_\n" for @ips;
+
+# Check with user to ensure not connecting to wrong servers;
+print "\nProceed? Type 'y' for yes\n";
+print "[no] ";
+my $proceed_ans = <STDIN>;
+if ( $proceed_ans !~ /^(y|yes|Y|YES)$/ ) {
+    print_out("\nExiting...\n");
+    exit(1);
+}
+
+# Check if Instances Exist;
+my $count = scalar @ips;
+if ( $count =~ /^0?$/ ) {
+    print "IP(s) Not Found!";
+    exit(0);
+}
+print STDERR "(debug) count: [$count]\n";
+
+# Determine Row / Column Layout;
+my $rows;
+if ( $count > 3 ) {
+    my $rows = sprintf "%.0f", sqrt($count);
+}
+else {
+    $rows = $count;
+}
+
+my $ips = join( ' ', @ips );
+
+print "\nRunning:\n";
+print "scripts/osascript.sh -o $ips -r $rows -u $USERNAME\n\n";
+`scripts/osascript.sh -o "$ips" -r $rows -u $USERNAME`;
+
+# Subroutines
+sub get_lines {
+    my $file = shift;
+    open( my $fh, '<', $file ) or die "Couldnt open file [$file]: $!";
+    chomp( my @lines = <$fh> );
+    close $fh;
+    return @lines;
+}
+
+sub validate_num_lines {
+    my @lines = @_;
+
+    my $length_input_file = scalar @lines;
+    my $lines_with_ips    = scalar( get_lines_with_ips(@lines) );
+
+    if ( $length_input_file != $lines_with_ips ) {
+        print -e "\n[error] Input file needs to be a list full of only servers with IP addresses. Each line is checked.\n";
+        usage();
+    }
+}
+
+sub get_lines_with_ips {
+    my @lines          = @_;
+    my @lines_with_ips = grep { /(\d{1,3}\.){3}\d{1,3}/ } @lines;
+    return @lines_with_ips;
+}
+
+sub get_ips {
+    my (%args) = @_;
+
+    my $include = ( defined $args{'include'} ) ? qr/$args{'include'}/ : qr/.*/;
+    my $exclude = ( defined $args{'exclude'} ) ? qr/$args{'exclude'}/ : qr/.*/;
+
+    return map { s/.+?((\d{1,3}\.){3}\d{1,3}).*/$1/r }
+      grep     { /$include/ }
+      grep     { !/$exclude/ } @{ $args{'lines'} };
+}
+
+exit(1);
+
+# Credit
+# https://github.com/ramannanda/Broadcast
+# https://github.com/bikboy/Tim/wiki/AWS-CLI----ITerm2-automation-(Mac)
+# https://starkandwayne.com/blog/bash-for-loop-over-json-array-using-jq/
+# https://alvinalexander.com/source-code/mac-os-x/how-run-multiline-applescript-script-unix-shell-script-osascript
---
+++ scripts/osascript.sh
@@ -0,0 +1,88 @@
+#!/bin/bash
+
+# Get the options
+while getopts ":ho:r:u:" option; do
+  case ${option} in
+    h ) usage ;;
+    o ) HOSTS=$OPTARG ;;
+    r ) ROWS=$OPTARG ;;
+    u ) USERNAME=$OPTARG ;;
+    \? )
+      echo "Invalid Option: -$OPTARG" 1>&2
+      exit 1
+      ;;
+  esac
+done
+shift $((OPTIND -1))
+
+# Start Terminal
+osascript <<EOF
+tell application "iTerm" to activate
+tell application "System Events" to tell process "iTerm" to keystroke "n" using command down
+EOF
+
+# Interate through Instances / Results
+i=0
+for HOST in $HOSTS; do
+
+    KEYPAIR="$HOME/.ssh/animal9"
+    MOD=$(($i%$ROWS))
+
+    if (( $i < $ROWS )); then
+    	# First Column - Split Horizontal to Init First of Each Row
+    	if [[ $i != 0 ]]; then
+    		osascript -e 'tell application "System Events" to tell process "iTerm" to keystroke "d" using {shift down, command down}'
+    	fi
+    else
+    	if [[ $MOD == 0 ]]; then
+    		# Loop Back Up to Top
+    		for ((j=1;j<$ROWS;j++)); do
+		    	osascript -e 'tell application "System Events" to tell process "iTerm" to key code 126 using {option down, command down}'
+			done
+    	else
+    		# Move Left / Move Down
+    		osascript -e 'tell application "System Events" to tell process "iTerm" to key code 123 using {option down, command down}'
+    		osascript -e 'tell application "System Events" to tell process "iTerm" to key code 125 using {option down, command down}'
+    	fi
+    	# Split Vertical
+		osascript -e 'tell application "System Events" to tell process "iTerm" to keystroke "d" using command down'
+    fi
+
+    # debug
+    # osascript -e 'tell application "System Events" to tell process "iTerm" to keystroke "ssh -i '"$KEYPAIR"' '"$USERNAME"'@'"$HOST"'"'
+    osascript -e 'tell application "System Events" to tell process "iTerm" to keystroke "ssh '"$USERNAME"'@'"$HOST"'"'
+	osascript -e 'tell application "System Events" to tell process "iTerm" to key code 52'
+
+    i=$(( $i + 1 ))
+done
+
+# Broadcast
+osascript -e 'tell application "System Events" to tell process "iTerm" to keystroke "i" using {option down, command down}'
+exit 0
+
+# I got this from
+# https://github.com/ramannanda/Broadcast
+#
+# I think I can use it legally as long as I keep this text with it, right?
+#
+# MIT License
+#
+# Copyright (c) 2019 Raman Nanda
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
